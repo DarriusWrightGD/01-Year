ab0ecb07f1d0e717cd841f7466993b9cbba1ef7a
DarriusWrightGD <darriuswrightgd@gmail.com>
2015-09-16 23:45:56 -0600

    Adding examples of auto and cleaning up repository

---
+++ C++/EffectiveModernC++/Auto/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+	auto myInt = 12;
+	auto myFloat = 43.0f;
+	auto myDouble = 123.0;
+	
+	cout << "Hello World" << endl;
+	return 0;
+}
