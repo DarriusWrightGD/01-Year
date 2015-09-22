5fd2ae05cabd9c30b595f3e164c2828dacd661a0
DarriusWrightGD <darriuswrightgd@gmail.com>
2015-09-21 21:46:55 -0600

    Adding new project RAndLValues

--- C++/EffectiveModernC++/Auto/Source.cpp
+++ C++/EffectiveModernC++/Auto/Source.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 
+//what is a unversial reference
+
+template<typename T>
+void foo( T && param)
+{
+	std::cout << param << std::endl;
+}
 
 int main()
 {
-	std::cout << "Hello World" << std::endl;
+	const int n1 = 34;
+	int foobar();
+	int j = foobar();
+
+	foo(j);
 	system("pause");
 	return 0;
+}
+
+int foobar()
+{
+	return 23;
 }
